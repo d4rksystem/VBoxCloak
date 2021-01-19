bb3672040da7a675f6ce3b39ae3ae48666a798fe
Kyle Cucci <kyle.cucci@gmail.com>
2021-01-19 16:24:07 +0100

    Added random string generator!
    Added random string generator, and replaced old static strings.

--- VBoxCloak.ps1
+++ VBoxCloak.ps1
@@ -1,10 +1,10 @@
 #################################################
-## VBoxCloak.ps1: A script that attempts to hide VirtualBox hypervisor from malware by modifying registry keys, killing associated processes, and removing uneeded driver/system files.
+## VBoxCloak.ps1: A script that attempts to hide the VirtualBox hypervisor from malware by modifying registry keys, killing associated processes, and removing uneeded driver/system files.
 ## Written and tested on Windows 7 System, but will likely work for Windows 10 as well.
 ## Many thanks to pafish for some of the ideas :) - https://github.com/a0rtega/pafish
 ##################################################
 ## Author: d4rksystem
-## Version: 0.3
+## Version: 0.4
 ##################################################
 
 # Define command line parameters
@@ -32,6 +32,18 @@ Write-Output 'procs: Kill processes.'
 Write-Output 'files: Make file system changes.'
 Write-Output 'Make sure to run as Admin!'
 Write-Output '*****************************************'
+
+# Define random string generator function
+function Get-RandomString {
+
+    $charSet = "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray()
+    
+    for ($i = 0; $i -lt 10; $i++ ) {
+        $randomString += $charSet | Get-Random
+    }
+
+    return $randomString
+}
 
 # Stop VBox Processes
 if ($procs) {
@@ -69,7 +81,7 @@ if ($reg) {
     if (Get-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "SystemBiosVersion" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Modifying Reg Key HKLM:\HARDWARE\Description\System\SystemBiosVersion..."
-	    Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "SystemBiosVersion" -Value "LOLBIOSv2"
+	    Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "SystemBiosVersion" -Value $(Get-RandomString)
 
     } Else {
 
@@ -81,9 +93,9 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SystemInformation" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Modifying Reg Key Values in HKLM:\SYSTEM\CurrentControlSet\Control\SystemInformation..."
-	    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SystemInformation" -Name "BIOSVersion" -Value "LOLBIOSv1"
-	    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SystemInformation" -Name "BIOSReleaseDate" -Value "12/01/2015"
-	    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SystemInformation" -Name "BIOSProductName" -Value "LOLBIOS"
+	    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SystemInformation" -Name "BIOSVersion" -Value $(Get-RandomString)
+	    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SystemInformation" -Name "BIOSReleaseDate" -Value $(Get-RandomString)
+	    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\SystemInformation" -Name "BIOSProductName" -Value $(Get-RandomString)
 
     } Else {
 
@@ -95,7 +107,7 @@ if ($reg) {
     if (Get-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "SystemBiosDate" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Modifying Reg Key HKLM:\HARDWARE\Description\System\SystemBiosDate"
-	    Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "SystemBiosDate" -Value "01/02/15"
+	    Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "SystemBiosDate" -Value $(Get-RandomString)
 
     } Else {
 
@@ -107,7 +119,7 @@ if ($reg) {
     if (Get-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "VideoBiosVersion" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Modifying Reg Key HKLM:\HARDWARE\Description\System\VideoBiosVersion"
-	    Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "VideoBiosVersion" -Value "VGALOLBIOSv2"
+	    Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "VideoBiosVersion" -Value $(Get-RandomString)
 
     } Else {
 
@@ -119,7 +131,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\SOFTWARE\Oracle\VirtualBox Guest Additions" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Renaming Reg Key HKLM:\SOFTWARE\Oracle\VirtualBox Guest Additions"
-	    Rename-Item -Path "HKLM:\SOFTWARE\Oracle\VirtualBox Guest Additions" -NewName "HidingFromTheMalwarez"
+	    Rename-Item -Path "HKLM:\SOFTWARE\Oracle\VirtualBox Guest Additions" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -131,7 +143,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\HARDWARE\ACPI\DSDT\VBOX__" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Renaming Reg Key HKLM:\HARDWARE\ACPI\DSDT\VBOX__"
-	    Rename-Item -Path "HKLM:\HARDWARE\ACPI\DSDT\VBOX__" -NewName "ACPISTUFF"
+	    Rename-Item -Path "HKLM:\HARDWARE\ACPI\DSDT\VBOX__" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -143,7 +155,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\HARDWARE\ACPI\FADT\VBOX__" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Renaming Reg Key HKLM:\HARDWARE\ACPI\FADT\VBOX__"
-	    Rename-Item -Path "HKLM:\HARDWARE\ACPI\FADT\VBOX__" -NewName "ACPISTUFF"
+	    Rename-Item -Path "HKLM:\HARDWARE\ACPI\FADT\VBOX__" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -155,7 +167,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\HARDWARE\ACPI\RSDT\VBOX__" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Renaming Reg Key HKLM:\HARDWARE\ACPI\RSDT\VBOX__"
-	    Rename-Item -Path "HKLM:\HARDWARE\ACPI\RSDT\VBOX__" -NewName "ACPISTUFF"
+	    Rename-Item -Path "HKLM:\HARDWARE\ACPI\RSDT\VBOX__" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -167,7 +179,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxMouse" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Renaming Reg Key HKLM:\SYSTEM\ControlSet001\services\VBoxMouse"
-	    Rename-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxMouse" -NewName "LOLMouse"
+	    Rename-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxMouse" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -179,7 +191,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxService" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Renaming Reg Key HKLM:\SYSTEM\ControlSet001\services\VBoxService"
-	    Rename-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxService" -NewName "LOLService"
+	    Rename-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxService" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -192,7 +204,7 @@ if ($reg) {
 
         Write-Output "[*] Renaming Reg Key HKLM:\SYSTEM\ControlSet001\services\VBoxSF"
 	    Write-Output "[!] Warning: This will disconnect VM shared folders. You will need to reconnect them later..."
-	    Rename-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxSF" -NewName "LOLSF"
+	    Rename-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxSF" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -204,7 +216,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxVideo" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Renaming Reg Key HKLM:\SYSTEM\ControlSet001\services\VBoxVideo"
-	    Rename-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxVideo" -NewName "LOLVideo"
+	    Rename-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxVideo" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -216,7 +228,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxGuest" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Renaming Reg Key HKLM:\SYSTEM\ControlSet001\services\VBoxGuest"
-	    Rename-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxGuest" -NewName "LOLGuest"
+	    Rename-Item -Path "HKLM:\SYSTEM\ControlSet001\services\VBoxGuest" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -228,7 +240,7 @@ if ($reg) {
     if (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "VBoxTray" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Renaming Reg Key HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\VBoxTray"
-	    Rename-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "VBoxTray" -NewName "LOLTray"
+	    Rename-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "VBoxTray" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -240,7 +252,7 @@ if ($reg) {
     if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Oracle VM VirtualBox Guest Additions" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Renaming Reg Key HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Oracle VM VirtualBox Guest Additions"
-	    Rename-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Oracle VM VirtualBox Guest Additions" -NewName "LOL LOL LOL"
+	    Rename-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Oracle VM VirtualBox Guest Additions" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -270,13 +282,12 @@ if ($files) {
     # We have to rename this file because we get errors when attempting to delete it! :o
 
     Write-Output '[*] Attempting to rename VBoxMRXNP.dll file...'
-    Rename-Item "C:\Windows\System32\VBoxMRXNP.dll" "C:\Windows\System32\not_today.dll"
+    Rename-Item "C:\Windows\System32\VBoxMRXNP.dll" "C:\Windows\System32\$(Get-RandomString).dll"
 
     # Rename VirtualBox folder path
 
     Write-Output '[*] Attempting to rename VirtualBox folder path...'
-    Rename-Item "C:\Program Files\Oracle\VirtualBox Guest Additions" "C:\Program Files\Oracle\detect dis"
+    Rename-Item "C:\Program Files\Oracle\VirtualBox Guest Additions" "C:\Program Files\Oracle\$(Get-RandomString)"
 }
 
 Write-Output '** Done! Did you recieve a lot of errors? Try running as Admin!'
-
